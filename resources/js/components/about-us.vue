e96438cb2323940584d430727a0d488a91885970
maciekz1996 <maciek3609@gmail.com>
2021-03-06 13:00:24 +0100

    Changed layout on LandingPage and added slideshow

--- resources/js/components/about-us.vue
+++ resources/js/components/about-us.vue
@@ -1,35 +1,40 @@
 <template>
-    <section class="hero is-fullheight" id="about-us">
-        <div class="hero-body">
-            <div class="container">
-                <div class="columns is-vcentered">
-                    <div class="column">
-                        <div class="column">
-                            <figure class="image">
-                                <img src="/images/page/piotrek.png">
-                            </figure>
-                        </div>
-                    </div>
-                    <div class="column is-5 about-us-main-column">
-                        <h1 class="about-us-title">{{ $t('messages.about_us') }}</h1>
-                        <div class="about-us-text-separator"></div>
-                        <h2 class="about-us-subtitle">
-                            {{ $t('messages.about-us-content') }}
-                        </h2>                       
-                        <div class="icon-parent">
-                            <span class="icon large-icon" v-scroll-to="'#gallery'">
-                                <i class="far fa-caret-square-down"></i>
-                            </span>                       
-                        </div>                          
-                    </div>
-                    <div class="column">
-                         <div class="column">
-                            <figure class="image">
-                                <img src="/images/page/mariusz.png">
-                            </figure>
-                        </div>                        
-                    </div>
-                </div>
+    <section class="about">
+        <div class="about__slideshow">
+            <img
+                src="/images/page/landing-page/3.jpg"
+                alt="About us"
+                class="slideshow__slide"
+                :class="{ 'slideshow__slide--active': currentImage === 3 }"
+            />
+            <img
+                src="/images/page/landing-page/2.jpg"
+                alt="About us"
+                class="slideshow__slide"
+                :class="{ 'slideshow__slide--active': currentImage === 2 }"
+            />
+            <img
+                src="/images/page/landing-page/1.jpg"
+                alt="About us"
+                class="slideshow__slide"
+                :class="{ 'slideshow__slide--active': currentImage === 1 }"
+            />
+            <div class="slideshow__progress">
+                <div
+                    v-for="i in imagesNumber"
+                    :key="i"
+                    class="progress__circle"
+                    :class="{ 'progress__circle--active': currentImage === i }"
+                />
+            </div>
+        </div>
+        <div class="about__content">
+            <h1 class="about__title">{{ $t("messages.about_us") }}</h1>
+            <p class="about__text">{{ $t("messages.about-us-content") }}</p>
+            <div class="about__icon">
+                <span class="icon large-icon" v-scroll-to="'#gallery'">
+                    <i class="far fa-caret-square-down"></i>
+                </span>
             </div>
         </div>
     </section>
@@ -39,68 +44,148 @@
 export default {
     data() {
         return {
-            animate: false
-        }
+            currentImage: 1,
+            imagesNumber: 3,
+            slideShowDelay: 4,
+            slideInterval: null
+        };
     },
-    methods: {
-        visibilityChanged (isVisible, entry) {            
-            if (isVisible) {
-                this.animate = true;
+    mounted() {
+        this.slideInterval = setInterval(() => {
+            if (this.currentImage < this.imagesNumber) {
+                this.currentImage += 1;
+            } else {
+                this.currentImage = 1;
             }
-        }
+        }, this.slideShowDelay * 1000);
+    },
+    unmounted() {
+        clearInterval(this.slideInterval);
     }
-}
+};
 </script>
 
 <style lang="scss">
-    @import '~@/_bulma-variables.scss';
-    @import '~@/_variables.scss';
+@import "~@/_bulma-variables.scss";
+@import "~@/_variables.scss";
 
-    .about-us-title {
-        font-size: 35pt !important;
-        font-weight: lighter !important;        
+.about {
+    max-width: 70%;
+    min-height: 90vh;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.about__slideshow {
+    width: 50%;
+    min-height: 600px;
+    height: fit-content;
+    position: relative;
+    padding-right: 2rem;
+}
+.slideshow__slide {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    max-height: 600px;
+    transform: translate(-50%, -50%);
+    transition: opacity 1s ease-in-out;
+    opacity: 0;
+    border-radius: 10px;
+}
+.slideshow__slide--active {
+    opacity: 1;
+}
+.slideshow__progress {
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    display: flex;
+    transform: translate(-50%, -50%);
+}
+.progress__circle {
+    border-radius: 50%;
+    background-color: #1a1a1a;
+    opacity: 0.5;
+    width: 16px;
+    height: 16px;
+    margin: 0 0.5rem;
+    transition: all 1s ease-in-out;
+}
+.progress__circle--active {
+    background-color: $accent-color;
+    opacity: 1;
+}
+
+.about__content {
+    width: 50%;
+    padding-left: 2rem;
+}
+.about__title {
+    font-size: 40px !important;
+    font-weight: 500 !important;
+    margin-bottom: 1rem;
+}
+.about__text {
+    font-size: 18px !important;
+    text-align: justify;
+    line-height: 1.9;
+}
+.large-icon {
+    font-size: 40pt;
+    margin-top: 50px;
+    color: $accent-color;
+    margin-left: 10px;
+}
+.large-icon:hover {
+    cursor: pointer;
+    opacity: 0.8;
+}
+.about__icon {
+    display: flex;
+    justify-content: center;
+}
+.about-us-text-separator {
+    background-color: $accent-color;
+    margin-bottom: 20px;
+    min-height: 2.5px;
+    width: 40% !important;
+}
+
+@media only screen and (max-width: 1280px) {
+    .about__title {
+        font-size: 25px !important;
+        font-weight: normal !important;
     }
-    .about-us-subtitle {
-        font-size: 14pt !important;
+    .about__text {
+        font-size: 16px !important;
         text-align: justify;
     }
-    .large-icon {
-        font-size: 40pt;
-        margin-top: 50px;        
-        color: $accent-color;
-        margin-left: 10px;
+    .about {
+        display: block;
     }
-    .large-icon:hover {
-        cursor: pointer;
-        opacity: 0.8;        
-    }
-    .icon-parent {
-        display: flex;
-        justify-content: center;
+    .about__slideshow {
         width: 100%;
+        margin: 0;
     }
-    .about-us-text-separator {
-        background-color: $accent-color;
-        margin-bottom: 20px;
-        min-height: 2.5px;
-        width: 40% !important;
-    }    
-    .about-us-main-column {
-        margin-left: 30px;
-        margin-right: 30px;
+    .about__content {
+        width: 100%;
+        padding: 0;
     }
-    @media only screen and (max-width: 1280px) {
-        .about-us-title {
-            font-size: 30pt !important;
-            font-weight: normal !important;        
-        }
-        .about-us-subtitle {
-            font-size: 12pt !important;
-            text-align: justify;
-        }
-        .about-us-image {
-            max-width: 80%;
-        }
+}
+@media only screen and (max-width: 700px) {
+    .about__slideshow {
+        min-height: 300px;
     }
 
+    .slideshow__slide {
+        max-height: 300px;
+    }
+
+    .about__text {
+        line-height: 1.5;
+    }
+}
 </style>
